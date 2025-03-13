c1b5717e7d9458f91d89513a2906deb598aedad1
William <williamwestergardd@gmail.com>
2025-03-13 23:30:32 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+fetch("https://github.com/williamwestergard/rest-api/api/users")
+  .then((response) => response.json())
+  .then((data) => {
+    let currentIndex = 0;
+
+    function updateContent() {
+      const user = data[currentIndex];
+      document.getElementById("name").innerText = user.name;
+      document.getElementById("desc").innerText = user.desc;
+      document.getElementById("value").innerText = user.value;
+      document.getElementById("price").innerHTML = user.price;
+      document.getElementById("image").src = "http://localhost:3000" + user.url;
+    }
+
+    updateContent();
+
+    document.getElementById("amethyst").addEventListener("click", () => {
+      currentIndex = 0; // Amethyst
+      updateContent();
+    });
+
+    document.getElementById("emerald").addEventListener("click", () => {
+      currentIndex = 1; // Emerald
+      updateContent();
+    });
+
+    document.getElementById("ruby").addEventListener("click", () => {
+      currentIndex = 2; // Ruby
+      updateContent();
+    });
+  });
